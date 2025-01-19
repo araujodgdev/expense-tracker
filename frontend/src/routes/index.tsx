82b0f4606a8715a993cf1c5d5c58a71bdcbad10d
Douglas Araújo <araujodgdev@gmail.com>
2025-01-18 22:24:44 -0300

    adds routing with tanstack router

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,52 @@
+import { createFileRoute } from '@tanstack/react-router'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { api } from '../lib/api'
+import { useQuery } from '@tanstack/react-query'
+
+export const Route = createFileRoute('/')({
+    component: Index,
+})
+
+async function fetchTotalSpent() {
+    // SIMULATES SERVER DELAY
+    // const simulatedPending = new Promise((res) => {
+    //   setTimeout(() => {
+    //     res("foo")
+    //   }, 1000)
+    // })
+    // await simulatedPending
+
+    const res = await api.expenses['total-spent'].$get()
+    if (!res.ok) {
+        throw new Error('Server error on fetching total amount spent')
+    }
+    const data = await res.json()
+    return data.result
+}
+
+function Index() {
+    const { isPending, error, data } = useQuery({ queryKey: ['get-total-spent'], queryFn: fetchTotalSpent, retry: false })
+
+    if (error) return 'An error has occurred: ' + error.message
+
+    return (
+        <div className='p-4'>
+            <Card className="max-w-md m-auto">
+                <CardHeader>
+                    <CardTitle>Total Spent</CardTitle>
+                    <CardDescription>Total amount spent.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    ${' '}{isPending ? '...' : data}
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
